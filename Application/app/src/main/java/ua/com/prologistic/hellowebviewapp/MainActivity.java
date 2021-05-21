bef14ed78e66f1fbd29f7251cdf82f1b79fb4e79
eugenelarionov <rus.greatness@gmail.com>
2021-05-21 22:22:34 +0300

    Init

---
+++ Application/app/src/main/java/ua/com/prologistic/hellowebviewapp/MainActivity.java
@@ -0,0 +1,37 @@
+package ua.com.prologistic.hellowebviewapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        SimpleWebViewClientImpl webViewClient = new SimpleWebViewClientImpl(this);
+        webView.setWebViewClient(webViewClient);
+
+        webView.loadUrl("http://bitibait.ru/");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && this.webView.canGoBack()) {
+            this.webView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+}
